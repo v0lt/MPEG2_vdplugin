3b4a6ff59a0508f7491c794dc357c31f06791f83
v0lt <mpc-be@mail.ru>
2026-08-30 10:00:24 +0300

    Добавлены compile.cmd и win32_compile.cmd.

---
+++ compile.cmd
@@ -0,0 +1,14 @@
+@ECHO OFF
+SETLOCAL
+
+FOR /f "delims=" %%A IN ('"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -property installationPath -latest -requires Microsoft.Component.MSBuild') DO SET VS_PATH=%%A
+
+SET DEVENV_PATH=%VS_PATH%\Common7\IDE\devenv
+
+ECHO Building the MPEG2_vdplugin Release x64.
+
+"%DEVENV_PATH%" MPEG2_vdplugin.sln /Rebuild "Release|x64"
+
+ENDLOCAL
+TIMEOUT /T 5
+EXIT /B
